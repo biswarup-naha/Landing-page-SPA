92add8cd69b22b6a2a6cf9888b5ee0fe65c80e55
biswarup-naha <jeeeeet6902@gmail.com>
2024-08-05 16:35:27 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,7 @@ import { Testimonials } from "./components/testimonials";
 import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
-import SmoothScroll from "smooth-scroll";
 import "./App.css";
-
-export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
-});
 
 const App = () => {
   const [landingPageData, setLandingPageData] = useState({});
